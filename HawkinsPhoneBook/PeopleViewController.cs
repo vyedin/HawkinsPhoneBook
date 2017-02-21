02ab27eb8c815121e6b4f6b56644beb77510b80d
Nina Vyedin <nina.vyedin@gmail.com>
2017-02-21 08:35:43 -0500

    clean up repo

--- HawkinsPhoneBook/PeopleViewController.cs
+++ HawkinsPhoneBook/PeopleViewController.cs
@@ -25,17 +25,8 @@ namespace HawkinsPhoneBook
 			Data["Wheeler"] = new[] { "Holly", "Karen", "Mike", "Nancy", "Ted" };
 			Data["Police"] = new[] { "Callahan", "Florence", "Hopper", "Powell" };
 
-#if DO_CYCLES
-			// This adds a cycle, as the *managed* PeopleViewController will have an [obj retain]
-			// on the *unmanaged* PeopleViewController, which will be holding on to the *unmanaged*
-			// peopleSource, which in turn will have a GCHandle to the *managed* 
-			// peopleSource, which references the *managed* PeopleViewController.
 			peopleTable.Source = new PeopleSource(this);
 
-#else
-			// this can be fixed by not referencing the controller from the Source:
-			peopleTable.Source = new PeopleSource (Data);
-#endif
 		}
 
 		public override void DidReceiveMemoryWarning()
@@ -47,32 +38,21 @@ namespace HawkinsPhoneBook
 
 	class PeopleSource : UITableViewSource
 	{
-#if DO_CYCLES
 		readonly PeopleViewController peopleViewController;
 
 		public PeopleSource(PeopleViewController peopleViewController)
 		{
 			this.peopleViewController = peopleViewController;
 		}
-#else
-		readonly Dictionary<string, string []> data;
-		public PeopleSource (Dictionary<string, string[]> data)
-		{
-			this.data = data;
-		}
-#endif
 
 		public override UITableViewCell GetCell(UITableView tableView, Foundation.NSIndexPath indexPath)
 		{
 			var cell = new UITableViewCell(UITableViewCellStyle.Default, indexPath.ToString());
 
 			string[] people = null;
-#if DO_CYCLES
+
 			if (peopleViewController.Data.TryGetValue(peopleViewController.Data.Keys.ElementAt((int)indexPath.Section), out people))
 			{
-#else
-			if (data.TryGetValue (data.Keys.ElementAt ((int) indexPath.Section), out people)) {
-#endif
 				var item = people[indexPath.Row];
 				cell.TextLabel.Text = item ?? "";
 			}
@@ -86,31 +66,19 @@ namespace HawkinsPhoneBook
 
 		public override nint NumberOfSections(UITableView tableView)
 		{
-#if DO_CYCLES
 			return peopleViewController.Data.Count;
-#else
-			return data.Count;
-#endif
 		}
 
 		public override string TitleForHeader(UITableView tableView, nint section)
 		{
-#if DO_CYCLES
 			return peopleViewController.Data.Keys.ElementAt((int)section);
-#else
-			return data.Keys.ElementAt ((int) section);
-#endif
 		}
 
 		public override nint RowsInSection(UITableView tableview, nint section)
 		{
 			string[] people = null;
-#if DO_CYCLES
 			if (peopleViewController.Data.TryGetValue(peopleViewController.Data.Keys.ElementAt((int)section), out people))
 			{
-#else
-			if (data.TryGetValue (data.Keys.ElementAt ((int) section), out people)) {
-#endif
 				return people.Length;
 			}
 
